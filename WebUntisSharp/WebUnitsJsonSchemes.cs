83d7c0daa1ec61f0062c6f5d32b4a4239d95988b
Marc Rousavy <Marc Rousavy>
2017-02-16 17:08:52 +0100

    Cleaned Code up and added Classes

--- WebUntisSharp/WebUnitsJsonSchemes.cs
+++ WebUntisSharp/WebUnitsJsonSchemes.cs
@@ -11,6 +11,13 @@ namespace WebUntisSharp {
 
             public class @params { }
         }
+
+        //Base-Class for all Responses
+        public class WebUntisResult {
+            public string id;
+            public object result;
+            public string jsonrpc = "2.0";
+        }
         //Parameter Class for JSON Query Method Parameters
         //public class @params {
         //    public string user = "ANDROID";
@@ -20,34 +27,106 @@ namespace WebUntisSharp {
         #endregion
 
         #region Individual Queries
-        //Authenticate the given user and start a session
-        public class Authentication : WebUntisQuery {
-            public string id;
-            public readonly string method = "authenticate";
+        namespace Sessions {
+            //Authenticate the given user and start a session
+            public class Authentication : WebUntisQuery {
+                public new string id;
+                public readonly new string method = "authenticate";
 
-            public class @params {
-                public string user = "ANDROID";
-                public string password;
-                public string client;
+                public new class @params {
+                    public string user = "ANDROID";
+                    public string password;
+                    public string client;
+                }
+            }
+
+            //Result of Authentication
+            public class AuthenticationResult : WebUntisResult {
+                public string sessionId;
+                public int personType;
+                public int personId;
+            }
+
+            //End the session
+            public class Logout : WebUntisQuery {
+                public new string id;
+                public new readonly string method = "logout";
             }
         }
 
-        //End the session
-        public class Logout : WebUntisQuery {
-            public string id;
-            public readonly string method = "logout";
+        namespace Teachers {
+            //Get list of teachers
+            public class GetTeachers : WebUntisQuery {
+                public new string id;
+                public new readonly string method = "getTeachers";
+            }
+
+            //Individual Teacher
+            public class Teacher {
+                public int id;
+                public string name;
+                public string foreName;
+                public string longName;
+                public string foreColor;
+                public string backColor;
+            }
+
+            //Result of Get Teachers Query
+            public class TeachersResult : WebUntisResult {
+                public new Teacher[] result;
+            }
         }
 
-        //Get list of teachers
-        public class Teachers : WebUntisQuery {
-            public string id;
-            public readonly string method = "getTeachers";
+        namespace Students {
+            //Get list of Students
+            public class GetStudents : WebUntisQuery {
+                public new string id;
+                public new readonly string method = "getStudents";
+            }
+
+            //Individual Student
+            public class Student {
+                public int id;
+                public string key;
+                public string name;
+                public string foreName;
+                public string longName;
+                //There are only 2 genders, why not use a bool
+                public string gender;
+            }
+
+            //Result of Get Students Query
+            public class StudentsResult : WebUntisResult {
+                public new Student[] result;
+            }
         }
 
-        //Get list of students
-        public class Students : WebUntisQuery {
-            public string id;
-            public readonly string method = "getStudents";
+        namespace Classes {
+            //Get Classes (Klassen) for schoolyear
+            public class GetClasses : WebUntisQuery {
+                public new readonly string method = "getKlassen";
+                public new class @params {
+                    string schoolyearId;
+                }
+            }
+
+            //Classes (Klassen) class
+            public class Class {
+                string name;
+                string longName;
+                string foreColor;
+                string backColor;
+                int did;
+            }
+
+            //Result of GetClasses
+            public class ClassesResult : WebUntisResult {
+                public new Class result;
+            }
+        }
+
+        namespace Subjects {
+
         }
         #endregion
     }
