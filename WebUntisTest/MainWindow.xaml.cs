50f2b4aefd7118f5cc362179bf5f6998e767c10a
mrousavy <marcrousavy@hotmail.com>
2017-02-20 12:19:03 +0100

    Updated Demo

--- WebUntisTest/MainWindow.xaml.cs
+++ WebUntisTest/MainWindow.xaml.cs
@@ -20,6 +20,26 @@ namespace WebUntisTest {
 
                 if(WebUntisSharp.WebUnitsJsonSchemes.LastError.Message != null) {
                     MessageBox.Show(WebUntisSharp.WebUnitsJsonSchemes.LastError.Message);
+                } else {
+                    var timegrid = _untis.GetTimegrid();
+                    var classregevents = _untis.GetClassRegEvents(01012015, 01012016);
+                    var classes = _untis.GetClasses();
+                    var departments = _untis.GetDepartments();
+                    var exams = _untis.GetExams(01012015, 01012016, 1);
+                    var holidays = _untis.GetHolidays();
+                    var lastimporttime = _untis.GetLastImportTime();
+                    var person = _untis.GetPersonId(5, "Rousavy", "Marc");
+                    var rooms = _untis.GetRooms();
+                    var schoolyear = _untis.GetSchoolyear();
+                    var schoolyears = _untis.GetSchoolyears();
+                    var statusdata = _untis.GetStatusData();
+                    var students = _untis.GetStudents();
+                    var teacher = _untis.GetTeachers();
+                    var subjects = _untis.GetSubjects();
+                    var substitution = _untis.GetSubstitution(01012015, 01012016);
+                    var timetable = _untis.GetTimetableForElement(0, 3, 01012015, 01012016);
+                    var sessionId = _untis.SessionId;
+                    Console.Write("Done.");
                 }
             } catch(Exception ex) {
                 MessageBox.Show(ex.Message);
